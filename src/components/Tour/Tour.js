dfaf10e3d4cdb9df410f8c35d6460bfcd0d51cfd
Haider <malikhaiderkhan81@gmail.com>
2023-12-20 21:40:48 +0500

    complete a site

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -2,27 +2,37 @@ import React, { Component } from "react";
 import "./Tour.scss";
 
 export default class Tour extends Component {
+  state = {
+    showInfo: false
+  };
+  handleInfo = () => {
+    this.setState({
+      showInfo: !this.state.showInfo
+    });
+  }
     render() {
+      const { id, city, img, name, info } = this.props.tour;
+      const { removeTour } = this.props;
         return (
             <article className="tour">
               <div className="img-container">
-                <img src="https://images.pexels.com/photos/2422588/pexels-photo-2422588.jpeg?auto=compress&cs=tinysrgb&w=600" alt="new york city" />
-                <span className="close-btn">
+                <img src={img} alt="new york city" />
+                <span className="close-btn" onClick={()=>{
+                  removeTour(id);
+                }}>
                     <i className="fas fa-window-close" />
                 </span>
               </div>
               <div className="tour-info">
-                <h3>city</h3>
-                <h4>name</h4>
+                <h3>{city}</h3>
+                <h4>{name}</h4>
                 <h5>
                   info{" "}
-                  <span>
+                  <span onClick={this.handleInfo}>
                     <i className="fas fa-caret-square-down" />
                   </span>
                 </h5>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, debitis.
-                </p>
+                {this.state.showInfo && <p>{info}</p>}
               </div>
             </article>
         )
